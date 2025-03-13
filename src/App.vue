09bda69ee5ff12298b3607d49702ede799be324b
Fernando Cueto <fernandoecueto@gmail.com>
2025-03-13 22:54:43 +0100

    update title in index.html, add DashboardView component, and modify App.vue to use it; update package.json and package-lock.json with new dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import HomeView from './views/HomeView.vue'
+  import DashboardView from './views/DashboardView.vue'
 </script>
 
 <template>
-  <HomeView />
+  <!-- <HomeView /> -->
+  <DashboardView/>
 </template>
 
 <style scoped>
