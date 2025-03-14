dfd34ba46600b23634c79d29afc6a78e2a5d8f9f
Fernando Cueto <fernandoecueto@gmail.com>
2025-03-14 19:34:36 +0100

    implement form validation and error handling in LoginView; update username input and loading state for login button

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,24 +1,23 @@
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-white">
-    <body class="h-full">
-    ```
-  -->
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-50 w-auto" src="@/assets/logo.png" alt="Your Company" />
+      <img class="mx-auto h-50 w-auto" src="@/assets/logo.png" alt="Curaduria Urbana 2 Barranquilla" />
       <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Conectese a su cuenta</h2>
     </div>
-
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+      <form class="space-y-6" @submit.prevent="handleLogin">
         <div>
-          <label for="email" class="block text-sm/6 font-medium text-gray-900">Usuario</label>
+          <label for="username" class="block text-sm/6 font-medium text-gray-900">Usuario</label>
           <div class="mt-2">
-            <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input 
+              v-model="username"
+              type="text" 
+              name="username" 
+              id="username" 
+              required
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              :class="{ 'outline-red-500': errors.username}" />
+            <p v-if="errors.username" class="mt-1 text-xs text-red-600">{{ errors.username }}</p>
           </div>
         </div>
 
@@ -30,19 +29,113 @@
             </div>
           </div>
           <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input 
+              v-model="password"
+              type="password" 
+              name="password" 
+              id="password" 
+              autocomplete="current-password" 
+              required 
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              :class="{ 'outline-red-500': errors.password}" />
+            <p v-if="errors.password" class="mt-1 text-xs text-red-600">{{ errors.password }}</p>
           </div>
         </div>
 
+        <div v-if="loginError" class="p-3 bg-red-100 text-red-700 rounded-md text-xs">
+          {{ loginError }}
+        </div>
+
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Inicio de Sesión</button>
+          <button 
+            type="submit" 
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            :disabled="isLoading"
+          >
+            <span v-if="isLoading">Cargando . . . </span>
+            <span v-else>Inicio de Sesión</span>
+          </button>
         </div>
       </form>
     </div>
   </div>
 </template>
 <script setup>
-  
+  import { ref, reactive } from 'vue'
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+
+  const username = ref('')
+  const password = ref('')
+  const errors = reactive({
+    username: false,
+    password: false
+  })
+  const loginError = ref('')
+  const isLoading = ref(false)
+
+  //validacion del formulario
+  const validateForm = () => {
+    let isValid = true
+    errors.username = ''
+    errors.password = ''
+
+    //validacion del email
+    if (!username.value) {
+      errors.username = 'El uusario es requerido'
+      isValid = false
+    }
+
+    //validacion del password
+    if (!password.value) {
+      errors.password = 'El password es requerido';
+      isValid = false;
+    } else if (password.value.length < 6) {
+      errors.password = 'El password debe tener al menos 6 caracteres';
+      isValid = false;
+    }
+
+    return isValid
+  }
+
+  const handleLogin = async () => {
+    if (!validateForm()) return
+
+    try {
+      isLoading.value = true
+      loginError.value = ''
+      const VITE_BASE_API_URL = import.meta.env.VITE_BASE_API_URL
+
+      const response = await fetch(`${VITE_BASE_API_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: username.value,
+          password: password.value
+        })
+      })
+
+      const data = await response.json()
+
+      if(data.status === 'error') {
+        loginError.value = data.message
+        throw new Error(data.message || 'Error en el inicio de sesión')
+      }
+
+      //Guardar el token en el store y localStorage
+      authStore.setToken(data.token);
+      authStore.setUser(username.value);
+
+      router.push('/home')
+    } catch (error) {
+      loginError.value = error.message || 'Error en el inicio de sesión'
+    } finally {
+      isLoading.value = false
+    }
+  }
 </script>
 <style scoped>
   
