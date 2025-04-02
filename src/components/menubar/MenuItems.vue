ca4296383e55d93a44e7fb5d403b0b8c47685513
Fernando Cueto <fernandoecueto@gmail.com>
2025-04-02 02:36:16 +0200

    feat(MenuItems): add navigation menu with logout button and active link highlighting

---
+++ src/components/menubar/MenuItems.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul role="list" class="-mx-2 space-y-1">
+    <li v-for="item in navigation" :key="item.name">
+      <RouterLink :to="item.href" :class="[isActive(item.href) ? 'bg-gray-800 text-white' : 'text-gray-400 hover:bg-gray-800 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold']">
+        <component :is="item.icon" class="size-6 shrink-0" aria-hidden="true" />
+        {{ item.name }}
+      </RouterLink>
+    </li>
+    <li>
+      <button @click="handleLogout" class="text-gray-400 hover:bg-gray-800 hover:text-white group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold cursor-pointer">
+        <ArrowRightStartOnRectangleIcon class="size-6 shrink-0"/>
+        Cerrar Sesión
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  import { computed } from 'vue'
+  import { ArrowRightStartOnRectangleIcon } from '@heroicons/vue/24/outline'
+  import { RouterLink, useRoute } from 'vue-router'
+  const { navigation } = defineProps(['navigation'])
+  const route = useRoute()
+
+  const isActive = (href) => {
+    return computed (() => route.name === href.name).value
+  }
+</script>
