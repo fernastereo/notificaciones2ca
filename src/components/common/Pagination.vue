e0efd412086f8d5e3cabfe6da83b171b583c5b9b
Fernando Cueto <fernandoecueto@gmail.com>
2025-04-12 00:34:42 +0200

    refactor(Pagination): Remove unnecessary class in pagination component to simplify layout

--- src/components/common/Pagination.vue
+++ src/components/common/Pagination.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+  <div class="flex items-center justify-between border-t border-gray-200 bg-white py-3">
     <div class="flex flex-1 justify-between sm:hidden">
       <a 
         href="#" 
