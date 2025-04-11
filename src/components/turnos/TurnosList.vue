c308a7fac7489edbdd852269dbddd19804e27fae
Fernando Cueto <fernandoecueto@gmail.com>
2025-04-11 22:43:36 +0200

    feat(TurnosList): Add a loading spinner and improve pagination management in the Turnos list

--- src/components/turnos/TurnosList.vue
+++ src/components/turnos/TurnosList.vue
@@ -20,61 +20,76 @@
     <div class="mt-8 flow-root">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <table class="min-w-full divide-y divide-gray-300">
-            <thead>
-              <tr>
-                <th scope="col" class="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">Fecha</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Turno</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Estado</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Responsable</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Dirección</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Teléfono</th>
-                <th scope="col" class="relative py-3.5 pr-4 pl-3 sm:pr-0">
-                  <span class="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200">
-              <tr v-for="expediente in turnos.expedientes" :key="expediente.id">
-                <td class="py-4 pr-3 pl-4 text-sm whitespace-nowrap text-gray-700 sm:pl-0">{{ `${expediente.fecha} - ${expediente.hora}` }}</td>
-                <td class="px-3 py-4 text-sm font-medium whitespace-nowrap text-gray-900">{{ `${expediente.numturno.toString().padStart(4, '0')}-${expediente.vigencia}` }}</td>
-                <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
-                  <Badge color="yellow">Revision Juridica</Badge>
-                </td>
-                <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-700">{{ expediente.responsables[0].nombre }}</td>
-                <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-700">{{ expediente.direccion }}</td>
-                <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-700">{{ expediente.responsables[0].telefono }}</td>
-                <td class="relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0">
-                  <RouterLink :to="'/home/turnos/' + expediente.id" class="text-indigo-600 hover:text-indigo-900">
-                    <PencilSquareIcon class="size-5" />
-                    <span class="sr-only">, {{ expediente.numturno }}</span>
-                  </RouterLink>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <LoadingSpinner v-if="isLoading" class="flex pt-32 justify-center items-center"/>
+          <template v-else>
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" class="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">Fecha</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Turno</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Estado</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Responsable</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Dirección</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Teléfono</th>
+                  <th scope="col" class="relative py-3.5 pr-4 pl-3 sm:pr-0">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200">
+                <tr v-for="expediente in turnos.expedientes" :key="expediente.id">
+                  <td class="py-4 pr-3 pl-4 text-sm whitespace-nowrap text-gray-700 sm:pl-0">{{ `${expediente.fecha} - ${expediente.hora}` }}</td>
+                  <td class="px-3 py-4 text-sm font-medium whitespace-nowrap text-gray-900">{{ `${expediente.numturno.toString().padStart(4, '0')}-${expediente.vigencia}` }}</td>
+                  <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
+                    <Badge color="yellow">Revision Juridica</Badge>
+                  </td>
+                  <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-700">{{ expediente.responsables[0].nombre }}</td>
+                  <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-700">{{ expediente.direccion }}</td>
+                  <td class="px-3 py-4 text-sm whitespace-nowrap text-gray-700">{{ expediente.responsables[0].telefono }}</td>
+                  <td class="relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0">
+                    <RouterLink :to="'/home/turnos/' + expediente.id" class="text-indigo-600 hover:text-indigo-900">
+                      <PencilSquareIcon class="size-5" />
+                      <span class="sr-only">, {{ expediente.numturno }}</span>
+                    </RouterLink>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <Pagination :pagination="turnos.pagination" @page-change="cambiarPagina" />
+          </template>
         </div>
-        <Pagination :pagination="turnos.pagination" @page-change="cambiarPagina" />
       </div>
     </div>
   </div>
 </template>
 <script setup>
+  import { ref } from 'vue'
   import Pagination from '@/components/common/Pagination.vue'
   import Badge from '@/components/common/Badge.vue'
+  import LoadingSpinner from '@/components/common/LoadingSpinner.vue'
   import { PlusIcon, PencilSquareIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
   import { RouterLink } from 'vue-router'
   import { useTurnos } from '@/composables/useTurnos'
   import { onMounted } from 'vue'
 
+  const isLoading = ref(false)
   const { getTurnos, turnos, apiError } = useTurnos()
 
+  const fetchTurnos = async (page = 1) => {
+    isLoading.value = true
+    try {
+      await getTurnos(page)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
   onMounted(async () => {
-    await getTurnos()
+    await fetchTurnos()
   })
 
   const cambiarPagina = async (nuevaPagina) => {
-    await getTurnos(nuevaPagina)
+    await fetchTurnos(nuevaPagina)
   };
 </script>
 
