eb261c1ad8c216779ede2e75464c70280095f482
Fernando Cueto <fernandoecueto@gmail.com>
2025-06-21 17:26:56 +0200

    feat(PersonRegistration): Añade cursor pointer al botón "Agregar Persona" para mejorar la interacción del usuario.

--- src/components/turnos/components/PersonRegistration.vue
+++ src/components/turnos/components/PersonRegistration.vue
@@ -57,7 +57,7 @@
     <div v-if="!showForm" class="mb-4">
       <button 
         @click="showAddForm" 
-        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
       >
         Agregar Persona
       </button>
