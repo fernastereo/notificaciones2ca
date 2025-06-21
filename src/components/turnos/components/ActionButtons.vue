292d1d6fed45eab0a906814e26e3489409d95478
Fernando Cueto <fernandoecueto@gmail.com>
2025-06-21 17:25:45 +0200

    feat(ActionButtons): Añade cursor pointer a los botones de cancelar y guardar para mejorar la interacción del usuario.

--- src/components/turnos/components/ActionButtons.vue
+++ src/components/turnos/components/ActionButtons.vue
@@ -3,14 +3,14 @@
     <button
       type="button"
       @click="$emit('cancel')"
-      class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 cursor-pointer"
     >
       Cancelar
     </button>
     <button
       type="button"
       @click="$emit('submit')"
-      class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
     >
       Guardar
     </button>
