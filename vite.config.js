0ab3ceb7afd847376e4f8a6deb8c30535ca208ae
Fernando Cueto <fernandoecueto@gmail.com>
2025-09-25 16:57:18 +0200

    updates vite config to handle multiples environments and updates workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-const base = import.meta.env.VITE_BASE_API_URL;
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-  base: base,
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  // Cargar variables de entorno
+  const pathBase = {
+    local: '/',
+    staging: '/2ca/admin/',
+    prod: '/admin/',
+  };
+  const env = loadEnv(mode, process.cwd(), '');
+  const server = env.VITE_DEPLOY_SERVER || 'local';
+  const base = pathBase[server] || '/';
+  console.log('🚀 ~ base:', base);
+
+  return {
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    base: base,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  };
 });
