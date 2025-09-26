c284f2be4b0984df73391b1ce3c69ec3842e105f
Fernando Cueto <fernandoecueto@gmail.com>
2025-09-26 14:02:27 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ command, mode }) => {
-  // Cargar variables de entorno
-  const pathBase = {
-    local: '/',
-    staging: '/2ca/admin/',
-    prod: '/admin/',
-  };
-  const env = loadEnv(mode, process.cwd(), '');
-  const server = env.VITE_DEPLOY_SERVER || 'local';
-  const base = pathBase[server] || '/';
-
-  return {
-    plugins: [vue(), vueDevTools(), tailwindcss()],
-    base: base,
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  };
+  },
 });
--- vitest.config.js
+++ vitest.config.js
@@ -1,16 +1,14 @@
-import { fileURLToPath } from 'node:url';
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
-import viteConfig from './vite.config';
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
 
-export default defineConfig(({ command, mode }) => {
-  return mergeConfig(
-    viteConfig({ command, mode }), // Call the function to get the config object
-    {
-      test: {
-        environment: 'jsdom',
-        exclude: [...configDefaults.exclude, 'e2e/**'],
-        root: fileURLToPath(new URL('./', import.meta.url)),
-      },
-    }
-  );
-});
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+)
